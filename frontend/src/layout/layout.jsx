ffcdb41683b85fe71ceba25993a009fa6065ae00
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-08 00:50:53 +0530

    imports

--- frontend/src/layout/layout.jsx
+++ frontend/src/layout/layout.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Routers from "../routes/Routers";
+
 const Layout = () => {
     return (
         <div>Layout</div>
