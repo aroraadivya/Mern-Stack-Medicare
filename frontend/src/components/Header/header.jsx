6a8bf5201393824af5e0d8cb7f548b0a0d255d9b
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-08 11:55:44 +0530

    header.jsx

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -1,5 +1,25 @@
-import React from 'react';
+import {useEffect, useRef} from 'react';
 import logo from '../../assets/images/logo.png';
+import { NavLink, Link } from 'react-router-dom';
+
+const navLinks = [
+    {
+        path:'/home',
+        display: 'Home'
+    },
+    {
+        path:'/doctors',
+        display: 'Find a Doctor'
+    },
+    {
+        path:'/services',
+        display: 'Services'
+    },
+    {
+        path:'/contact',
+        display: 'Contact'
+    },
+]
 
 const Header = () => {
     return <header className="header flex items-center">
@@ -8,6 +28,19 @@ const Header = () => {
                 {/* logo */}
                 <div>
                     <img src={logo} alt=""/>
+                </div>
+
+                {/* Menu */}
+                <div classNme="navigation">
+                    <ul classNme="menu flex items-center gap-[2.7rem]">
+                        {
+                            navLinks.map((link, index) => {
+                                return <li key={index}>
+                                    <NavLink to={link.path} className={navClass=> nacClass.isActive ? 'text-primaryColor text-[16px] leading-7 font-[600]' : 'text-textColor text-[16px] leading-7 font[500]'}>{link.display}</NavLink>
+                                </li>
+                            })
+                        }
+                    </ul>
                 </div>
             </div>
         </div>
