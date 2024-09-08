8693e6acc6e49543a57b8f5196c4506f30aa15d0
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-08 11:44:06 +0530

    header

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import logo from '../../assets/images/logo.png';
 
 const Header = () => {
-    return (
-        <div>Header</div>
-    );
+    return <header className="header flex items-center">
+        <div className="container">
+            <div className="flex items-center justify-between">
+                {/* logo */}
+                <div>
+                    <img src={logo} alt=""/>
+                </div>
+            </div>
+        </div>
+    </header>
 }
 
 export default Header;
