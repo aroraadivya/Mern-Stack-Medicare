e5a1737cb20f17aec7acb34f9140f8c81218300a
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-08 10:46:03 +0530

    doctorsdetails.jsx

---
+++ frontend/src/pages/Doctors/DoctorsDetails.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DoctorsDetails = () => {
+    return (
+        <div>
+            <h1>DoctorsDetails</h1>
+        </div>
+    );
+};
+
+export default DoctorsDetails;
