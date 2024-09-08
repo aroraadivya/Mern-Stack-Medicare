8b4a64d18b36aacb9d36b42b5f0bd92ae5c2e74e
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-08 17:24:04 +0530

    header.jsx

--- frontend/src/components/Header/header.jsx
+++ frontend/src/components/Header/header.jsx
@@ -1,6 +1,8 @@
 import {useEffect, useRef} from 'react';
 import logo from '../../assets/images/logo.png';
+import userImg from '../../assets/images/avatar-icon.png'
 import { NavLink, Link } from 'react-router-dom';
+import { BiMenu } from 'react-icons/bi';
 
 const navLinks = [
     {
@@ -36,11 +38,30 @@ const Header = () => {
                         {
                             navLinks.map((link, index) => {
                                 return <li key={index}>
-                                    <NavLink to={link.path} className={navClass=> navClass.isActive ? 'text-primaryColor text-[16px] leading-7 font-[600]' : 'text-textColor text-[16px] leading-7 font[500]'}>{link.display}</NavLink>
+                                    <NavLink to={link.path} className={navClass=> navClass.isActive ? 'text-primaryColor text-[16px] leading-7 font-[600]' : 'text-textColor text-[16px] leading-7 font[500] hover:text-primaryColor'}>{link.display}</NavLink>
                                 </li>
                             })
                         }
                     </ul>
+                </div>
+
+                {/* nav right */}
+                <div className="flex items-center gap-4">
+                    <div>
+                        <Link to='/'>
+                            <figure className='w-[35px] h-[35px] rounded-full cursor-pointer'>
+                                <img src={userImg} className='w-full rounded-full' alt=''/>
+                            </figure>
+                        </Link>
+                    </div>
+
+                    <Link to='/login'>
+                        <button className='bg-primaryColor py-2 px-6 text-white font-[600] h-[44px] flex items-center justify-center rounded-[50px]'>Login</button>
+                    </Link>
+
+                    <span className='md:hidden'>
+                        <BiMenu className='w-6 h-6 cursor-pointer'/>
+                    </span>
                 </div>
             </div>
         </div>
