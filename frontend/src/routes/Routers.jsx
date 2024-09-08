04435415b16f20a1a584ac19093f397c40a88440
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-08 11:17:06 +0530

    imports

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import Home from '../pages/Home';
+import Services from '../pages/Services';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
+import Contact from '../pages/Contact';
+import Doctors from '../pages/Doctors/Doctors';
+import DoctorDetails from '../pages/Doctors/DoctorDetails'; 
 
+import { Routes, Route } from 'react-router-dom';
 const Routers = () => {
     return (
         <div>Routers</div>
