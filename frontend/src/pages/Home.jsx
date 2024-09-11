c4d8d5c30b26d24c0d3b24c4f807f5f498442457
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-11 13:43:26 +0530

    home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -27,7 +27,24 @@ const Home = () => {
                             <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>
                                 30+
                             </h2>
-                            <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span> 
+                            <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
+                            <p className='text__para'>Years Of Experience</p>
+                        </div>
+
+                        <div>
+                            <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>
+                                30+
+                            </h2>
+                            <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
+                            <p className='text__para'>Years Of Experience</p>
+                        </div>
+
+                        <div>
+                            <h2 className='text-[36px] leading-[56px] lg:text-[44px] lg:leading-[54px] font-[700] text-headingColor'>
+                                30+
+                            </h2>
+                            <span className='w-[100px] h-2 bg-yellowColor rounded-full block mt-[-14px]'></span>
+                            <p className='text__para'>Years Of Experience</p>
                         </div>
                     </div>
                 </div>
