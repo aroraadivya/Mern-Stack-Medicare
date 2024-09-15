1d4118d983de6e634199ad262e035684bbe8e8fb
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-15 23:49:20 +0530

    upd

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,6 @@ const Home = () => {
             <div className='flex flex-col lg:flex-row gap-[90px] items-center justify-between'>
                 {/* hero content */}
                 <div>
-                    
                     <div className='lg:w-[570px]'>
                         <h1 className='text-[36px] leading-[46px] text-headingColor font-[800] md:text-[60px]
                         md:leading-[70px]'>We help patients live a healthy, longer life.
