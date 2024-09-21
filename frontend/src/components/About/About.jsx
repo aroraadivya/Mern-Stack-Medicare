67dc75ee39a2d715cf9d36694027c97dd138bdf3
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-21 19:44:06 +0530

    about

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const About = () => {
-    return <div>About</div>
+    return <section>
+        <div className="container">
+            <div className="flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row"></div>
+        </div>
+    </section>
 };
 
 export default About;
