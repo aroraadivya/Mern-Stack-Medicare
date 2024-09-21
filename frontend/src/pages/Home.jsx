24f75b83ec0e9ad8fddb2163fa22413a74c73d00
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-21 19:34:06 +0530

    icons

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,8 +3,8 @@ import heroImg01 from '../assets/images/hero-img01.png';
 import heroImg02 from '../assets/images/hero-img02.png';
 import heroImg03 from '../assets/images/hero-img03.png';
 import icon01 from '../assets/images/icon01.png';
-import icon02 from '../assets/images/icon01.png';
-import icon03 from '../assets/images/icon01.png';
+import icon02 from '../assets/images/icon02.png';
+import icon03 from '../assets/images/icon03.png';
 import { Link } from 'react-router-dom';
 import { BsArrowRight } from 'react-icons/bs';
 
