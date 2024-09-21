60ddb8057ade5d9d57fdf3776571955e7fab8a2f
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-21 20:32:44 +0530

    card img

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
                 {/* about img */}
                 <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1">
                     <img src={aboutImg} alt=""/>
-                    <div className="absolute z-20 bottom-4 w-[200px] md:w-[300px]">
+                    <div className="absolute z-20 bottom-4 w-[200px] md:w-[300px] right-[-30%] md:right-[-7%] lg:right-[-22%]">
                         <img src={aboutCardImg} alt=""/>
                     </div>
                 </div>
