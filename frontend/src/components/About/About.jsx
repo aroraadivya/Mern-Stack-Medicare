3e917f4cd85331efd15a40bfcf0ae5ef72519aa3
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-21 19:52:28 +0530

    about.png

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import aboutImg from "../../assets/images/about.png";
 
 const About = () => {
     return <section>
         <div className="container">
-            <div className="flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row"></div>
+            <div className="flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row">
+                {/* about img */}
+                <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1">
+                    <img src={aboutImg} alt=""></img>
+                </div>
+            </div>
         </div>
     </section>
 };
