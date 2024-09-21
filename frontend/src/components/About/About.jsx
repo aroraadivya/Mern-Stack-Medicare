1e19c26a71bd8f8bbef9e4475e218a5a2f7e3268
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-21 19:57:48 +0530

    about

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
             <div className="flex justify-between gap-[50px] lg:gap-[130px] xl:gap-0 flex-col lg:flex-row">
                 {/* about img */}
                 <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1">
-                    <img src={aboutImg} alt=""></img>
+                    <img src={aboutImg} alt=""/>
                 </div>
             </div>
         </div>
