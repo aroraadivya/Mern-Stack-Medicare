1919ff69fd4711bfc981f7fb9a921576f8f43b59
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-21 20:30:36 +0530

    about card image

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import aboutImg from "../../assets/images/about.png";
+import aboutCardImg from "../../assets/images/about-card.png";
 
 const About = () => {
     return <section>
@@ -8,6 +9,9 @@ const About = () => {
                 {/* about img */}
                 <div className="relative w-3/4 lg:w-1/2 xl:w-[770px] z-10 order-2 lg:order-1">
                     <img src={aboutImg} alt=""/>
+                    <div className="absolute z-20 bottom-4 w-[200px] md:w-[300px]">
+                        <img src={aboutCardImg} alt=""/>
+                    </div>
                 </div>
             </div>
         </div>
