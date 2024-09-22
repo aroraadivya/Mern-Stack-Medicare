6662574cbbbd7550d522374b5ca3419f330d34cf
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-22 19:59:38 +0530

    home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -146,6 +146,19 @@ const Home = () => {
             </section>
 
             <About/>
+
+            {/* Services section starts */}
+
+            <section>
+                <div className='container'>
+                    <div className='xl:w-[470px] mx-auto'>
+                        <h2 className='heading text-center'>Our medical services</h2>
+                        <p className='text__para text-center'>World-class care for everyone. Our health system offers unmatched, expert health care.</p>
+                    </div>
+                </div>
+            </section>
+
+            {/* Services section ends */}
             
         </>
 
