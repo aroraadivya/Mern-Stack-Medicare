7c18b737220374e1b8b46e18ac8e7bbf44c7ffaa
DIVYA ARORA <111882666+divya0o1@users.noreply.github.com>
2024-09-22 20:19:49 +0530

    service card content

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const Servicecard = () =>{
-    return <div>Servicecard</div>
-};
+const ServiceCard = ({ item, index }) => {
+    const { name, desc, bgColor, textColor } = item;
 
-export default Servicecard;
+    return (
+        <div className='py-[30px] px-3 lg:px-5'>
+            <h2 className='text-[26px] leading-9 text-headingColor font-[700]'>
+                {name}
+            </h2>
+            <p className='text-[16px] leading-7 fomt-[400] text-textColor mt-4'>
+                {desc}
+            </p>
+        </div>
+    )
+}
--- frontend/src/components/Services/ServiceList.jsx
+++ frontend/src/components/Services/ServiceList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import {services} from './../../assets/data/services';
+import { services } from './../../assets/data/services';
 import ServiceCard from './ServiceCard';
 
 
-const ServiceList = () =>{
+const ServiceList = () => {
     return <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
-        {services.map((service, index) => <ServiceCard item={item} index={index} key={index}/>)}
+        {services.map((service, index) => <ServiceCard item={item} index={index} key={index} />)}
     </div>
 };
 
