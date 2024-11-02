c132009f9921924192b71ccba2199dc7bde466a7
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-02 19:34:40 +0530

    Add Link import to ServiceCard component

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BsArrowRight } from "react-icons/bs";
+import {  Link } from "react-router-dom"
 
 const ServiceCard = ({ item, index }) => {
     const { name, desc, bgColor, textColor } = item;
