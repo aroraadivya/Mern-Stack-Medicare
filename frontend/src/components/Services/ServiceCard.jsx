5f960bcc713cd04006e6dbe2b6fc51e37471b9f2
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-02 19:32:07 +0530

    Add link to doctors page in ServiceCard component

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -13,6 +13,9 @@ const ServiceCard = ({ item, index }) => {
             </p>
 
             <div className='flex items_center justify-between mt-[30px]'>
+                <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center group hover:bg-primaryColor hover:border-none'>
+                    <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+                </Link>
             </div>
         </div>
     )
