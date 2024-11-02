e1275253d24a037a875641ec33c08637c52af6e1
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-02 21:10:56 +0530

    Refactor ServiceCard component into ServiceList and update mapping to use services data

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -1,25 +1,12 @@
 import React from 'react';
-import { BsArrowRight } from "react-icons/bs";
-import {  Link } from "react-router-dom"
+import { services } from './../../assets/data/services';
+import ServiceCard from './ServiceCard.jsx';
 
 
-const ServiceCard = ({ item, index }) => {
-    const { name, desc, bgColor, textColor } = item;
+const ServiceList = () => {
+    return <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
+        {services.map((service, index) => <ServiceCard item={item} index={index} key={index} />)}
+    </div>
+};
 
-    return (
-        <div className='py-[30px] px-3 lg:px-5'>
-            <h2 className='text-[26px] leading-9 text-headingColor font-[700]'>
-                {name}
-            </h2>
-            <p className='text-[16px] leading-7 fomt-[400] text-textColor mt-4'>
-                {desc} 
-            </p>
-
-            <div className='flex items_center justify-between mt-[30px]'>
-                <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center group hover:bg-primaryColor hover:border-none'>
-                    <BsArrowRight className='group-hover:text-white w-6 h-5'/>
-                </Link>
-            </div>
-        </div>
-    )
-}
+export default ServiceList;
