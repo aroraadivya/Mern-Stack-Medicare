cca637dbcd9b34c90a90c3cd8a80a6a57b285b55
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-05 20:42:25 +0530

    Add "Learn More" button to Home component for improved navigation

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -175,6 +175,10 @@ const Home = () => {
                             <li className='text__para'>2. Search for your physician here, and contact their office.</li>
                             <li className='text__para'>3. View our physicians who are accepting new patients, use the online scheduling tool to select an appointment time.</li>
                         </ul>
+
+                        <Link to="/">
+                            <button className='btn'>Learn More</button>
+                        </Link>
                     </div>
                 </div>
             </section>
