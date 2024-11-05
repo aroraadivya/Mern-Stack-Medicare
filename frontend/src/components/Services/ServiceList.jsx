317bb8388baf647ac59245792706ae0f1475eaac
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-05 19:23:10 +0530

    Update ServiceCard import in ServiceList and integrate ServiceList into Home component

--- frontend/src/components/Services/ServiceList.jsx
+++ frontend/src/components/Services/ServiceList.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { services } from './../../assets/data/services';
-import ServiceCard from './ServiceCard';
+import ServiceCard from './ServiceCard.jsx';
 
 const ServiceList = () => {
     return<div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px]'>
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -157,12 +157,10 @@ const Home = () => {
                         <h2 className='heading text-center'>Our medical services</h2>
                         <p className='text__para text-center'>World-class care for everyone. Our health system offers unmatched, expert health care.</p>
                     </div>
+                    <ServiceList />
                 </div>
             </section>
-
             {/* Services section ends */}
-            
-            < ServiceCard />
         </>
 
     </>
