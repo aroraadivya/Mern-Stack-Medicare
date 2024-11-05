0fa1034d7b3c1ec238ee408990535c2088f539ca
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-05 19:46:59 +0530

    Refactor ServiceCard component for improved readability and styling consistency

--- frontend/src/components/Services/ServiceCard.jsx
+++ frontend/src/components/Services/ServiceCard.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { BsArrowRight } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
-const ServiceCard = ({item, index}) => {
-    const {name, desc, bgColor, textColor} = item
+const ServiceCard = ({ item, index }) => {
+    const { name, desc, bgColor, textColor } = item
 
     return <div className="py-[30px] px-3 lg:px-5">
         <h2 className="text-[26px] leading-9 text-headingColor font-[700]">{name}</h2>
@@ -11,10 +11,10 @@ const ServiceCard = ({item, index}) => {
 
         <div className="flex items-center justify-center mt-[30px]">
             <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center group hover:bg-primaryColor hover:border-none'>
-                <BsArrowRight className='group-hover:text-white w-6 h-5'/>
+                <BsArrowRight className='group-hover:text-white w-6 h-5' />
             </Link>
 
-            <span className="w-[44px] h-[44px] flex items-center justify-center text-[18px] leading-[30px] font-[600]" style={{background:`${bgColor}`}}>
+            <span className="w-[44px] h-[44px] flex items-center justify-center text-[18px] leading-[30px] font-[600]" style={{ background: `${bgColor}`, color: '${textColor}', borderRadius: "6px 0 0 6px" }}>
                 {index + 1}
             </span>
         </div>
