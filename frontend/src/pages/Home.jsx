b74f7fce883e4a39825b46d338a3653b16cdf2c5
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-08 20:00:00 +0530

    Update heading in Home component to enhance clarity on services offered

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -221,7 +221,7 @@ const Home = () => {
             <section>
                 <div className='container'>
                     <div className='xl:w-[470px] mx-auto'>
-                        <h2 className='heading text-center'>Our medical services</h2>
+                        <h2 className='heading text-center'>Our great doctors</h2>
                         <p className='text__para text-center'>World-class care for everyone. Our health system offers unmatched, expert health care.</p>
                     </div>
                 </div>
