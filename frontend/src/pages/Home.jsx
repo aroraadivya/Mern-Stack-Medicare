749aa68fda76f6a3a1857424423cb4dc95963647
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-08 19:52:24 +0530

    Add new line for improved code readability in Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,6 +13,7 @@ import ServiceCard from '../components/Services/ServiceCard';
 import featureImg from '../assets/images/feature-img.png';
 import videoIcon from '../assets/images/video-icon.png';
 import avatarIcon from '../assets/images/avatar-icon.png';
+
 const Home = () => {
     return <>
         {/* hero section */}
