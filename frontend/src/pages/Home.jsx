5c283c85a5894bfc488fbd5b3607fedc2d0950ed
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-08 19:59:00 +0530

    Add heading and description for medical services section in Home component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -219,7 +219,12 @@ const Home = () => {
 
             {/* Our Great Doctors start*/}
             <section>
-                <div className='container'></div>
+                <div className='container'>
+                    <div className='xl:w-[470px] mx-auto'>
+                        <h2 className='heading text-center'>Our medical services</h2>
+                        <p className='text__para text-center'>World-class care for everyone. Our health system offers unmatched, expert health care.</p>
+                    </div>
+                </div>
             </section>
             {/* Our great dotors section end */}
         </>
