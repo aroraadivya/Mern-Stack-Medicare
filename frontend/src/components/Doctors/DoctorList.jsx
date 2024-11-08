a6de76fc7454096cef0e5560a50fe6b5a4f8b150
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-08 20:02:50 +0530

    Add DoctorList component to display list of doctors

---
+++ frontend/src/components/Doctors/DoctorList.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { doctors } from './../../assets/data/doctors'
+
+const DoctorList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default DoctorList
