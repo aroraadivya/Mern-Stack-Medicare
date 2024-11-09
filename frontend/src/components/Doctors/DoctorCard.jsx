f9f255d693bd2260e0d3aba15125c866aeed8f9a
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-09 14:41:19 +0530

    Add width styling to photo in DoctorCard component

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -7,7 +7,7 @@ const DoctorCard = ({doctor}) => {
   return (
     <div className='p-3 lg:p-5'>
       <div>
-        <img src={photo} alt=''></img>
+        <img src={photo} className='w-full' alt=''></img>
       </div>
     </div>
   )
