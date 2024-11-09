873034174cd26920cfd0e77f6b46b17159890738
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-09 15:01:21 +0530

    Update DoctorCard component to style specialization label with background and text colors

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -18,7 +18,7 @@ const DoctorCard = ({doctor}) => {
       </h2>
 
       <div className='mt-2 lg:mt-4 flex items-center justify-between'>
-        <span>
+        <span className='bg-[#CCF0F3] text-irisBlueColor py-1 px-2 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded'>
           {specialization}
         </span>
       </div>
