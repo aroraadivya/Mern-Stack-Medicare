004cd9e98542824adc91e3d25b95a56010a6698f
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-09 15:14:36 +0530

    Added empty h3 element inside a div in DoctorCard component.

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -31,7 +31,9 @@ const DoctorCard = ({doctor}) => {
       </div>
 
       <div className='mt-[18px] lg:mt-5 flex items-center justify-between'>
-        
+        <div>
+          <h3 className='text-[16px] leading-7 lg:text-[18px] lg:leading-[30px] font-semibold'></h3>
+        </div>
       </div>
     </div>
   );
