e7edcabec62fb6fc0e37daec85c1fed82a9a160b
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-09 15:04:41 +0530

    Added an empty div with flex and items-center classes, and a span with font-semibold class inside DoctorCard component.

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -21,6 +21,10 @@ const DoctorCard = ({doctor}) => {
         <span className='bg-[#CCF0F3] text-irisBlueColor py-1 px-2 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded'>
           {specialization}
         </span>
+
+        <div className='flex items-center gap-[6px]'>
+          <span className='flex items-center gap-[6px] text-[14px] leading-6 lg:text-[16px] leading-7 font-semibold text-headingColor'></span>
+        </div>
       </div>
     </div>
   )
