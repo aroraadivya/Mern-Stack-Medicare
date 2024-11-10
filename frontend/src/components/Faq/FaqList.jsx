ba62fb709db909a8bb6f6392dd30065eb30c62ef
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-10 15:23:30 +0530

    Fix incorrect reference to FaqList in FaqList component

--- frontend/src/components/Faq/FaqList.jsx
+++ frontend/src/components/Faq/FaqList.jsx
@@ -5,9 +5,9 @@ import FaqItem from './FaqItem';
 const FaqList = () => {
   return (
     <ul className='mt-[38px]'>
-        {FaqList.map((item,index)=> <FaqItem item={item} key={index}/>)}
+        {faqs.map((item, index)=> <FaqItem item={item} key={index}/>)}
     </ul>
-  )
-}
+  );
+};
 
 export default FaqList;
