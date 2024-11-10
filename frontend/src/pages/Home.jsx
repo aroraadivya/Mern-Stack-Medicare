16fd055f21d59fed012a7ea002cbbc464924e58c
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-10 15:19:42 +0530

    Add FaqList component to Home page for displaying patient FAQs

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -238,8 +238,10 @@ const Home = () => {
                     <div className='flex justify-between gap=[50px] lg:gap-0'>
                         <div className='w-1/2 hidden md:block'><img src={faqImg} alt=''/></div>
 
-                        <div>
+                        <div className='w-full md:w-1/2'>
                             <h2 className='heading'>Most questions by our beloved patients.</h2>
+
+                            <FaqList />
                         </div>
                     </div>
                 </div>
