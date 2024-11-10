53654c36e64e1875a8d15bcd77fee8003008a924
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-10 14:45:17 +0530

    Enhance Home component: updated Link component to include 'justify-center' class for improved alignment

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -105,7 +105,7 @@ const Home = () => {
                                 World-class care for everyone. Our health system offers unmatched, expert health care.From the lab to the clinic.
                             </p>
 
-                            <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center group hover:bg-primaryColor hover:border-none'>
+                            <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center justify-center group hover:bg-primaryColor hover:border-none'>
                                 <BsArrowRight className='group-hover:text-white w-6 h-5'/>
                             </Link>
                         </div>
