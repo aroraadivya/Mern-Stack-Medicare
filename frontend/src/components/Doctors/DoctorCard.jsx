e5fe6f201549f0e25e87699a203b72abaefca43b
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-10 14:43:02 +0530

    "Updated DoctorCard component: modified Link component with new props and added BsArrowRight icon"

--- frontend/src/components/Doctors/DoctorCard.jsx
+++ frontend/src/components/Doctors/DoctorCard.jsx
@@ -36,7 +36,8 @@ const DoctorCard = ({doctor}) => {
           <p className='text-[14px] leading-6 font-[400] text-textColor'>At {hospital}</p>
         </div>
 
-        <Link to='/doctors/1'>
+        <Link to='/doctors' className='w-[44px] h-[44px] rounded-full border border-solid border-[#181A1E] mt-[30px] mx-auto flex items-center group hover:bg-primaryColor hover:border-none'>
+          <BsArrowRight className='group-hover:text-white w-6 h-5' />
         </Link>
       </div>
     </div>
