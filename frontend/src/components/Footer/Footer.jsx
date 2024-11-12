547dd5c83fbc27f00100fb6c1d19549137c96c9f
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 20:54:40 +0530

    Add LinkedIn icon import to Footer component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'; 
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
+import { RiLinkedinFill } from 'react-icons/ri';
 
 const Footer = () => {
     return (
