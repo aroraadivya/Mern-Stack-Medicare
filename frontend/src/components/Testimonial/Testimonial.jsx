efe0771379f12f70ba4c53b444be4f1fc5fcaccf
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 18:06:43 +0530

    Remove unused SwiperSlide component from Testimonial section

--- frontend/src/components/Testimonial/Testimonial.jsx
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -26,18 +26,7 @@ const Testimonial = () => {
         }}>
         </Swiper>
 
-        <SwiperSlide>
-            <div className='py-[30px] px-5 rounded-3'>
-                <div className='flex items-center gap-[13px]'>
-                    <img src={patientAvatar} alt=''/>
-                    <div>
-                        <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor'>
-                            Muhbur Rahman
-                        </h4>
-                    </div>
-                </div>
-            </div>
-        </SwiperSlide>
+        
     </div>
   )
 }
