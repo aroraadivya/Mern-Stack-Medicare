3c9e38896ebf2449ff39254f1c26ff49c02b3694
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 14:43:08 +0530

    Fix toggle function in FaqItem component to correctly open and close accordion

--- frontend/src/components/Faq/FaqItem.jsx
+++ frontend/src/components/Faq/FaqItem.jsx
@@ -5,17 +5,16 @@ import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 const FaqItem = ({item}) => {
 
-
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleAccordion = () => {
-        setIsOpen(true);
+        setIsOpen(!isOpen);
     };
 
-    
+
     return (
     <div className='p-3 lg:p-5 rounded-[12px] border border-solid border-[#D9DCE2] mb-5 cursor-pointer'>
-        <div className='flex items-center justify-between gap-5'>
+        <div className='flex items-center justify-between gap-5' onClick={toggleAccordion}>
             <h4 className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor'>
                 {item.question}
             </h4>
