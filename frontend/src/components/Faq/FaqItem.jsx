e6f83de13380adb07358c570cf64650a6a00b7da
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 14:23:29 +0530

    Refactor FaqItem component to accept props and update structure

--- frontend/src/components/Faq/FaqItem.jsx
+++ frontend/src/components/Faq/FaqItem.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 import FaqList from './FaqList';
 import { useState } from 'react';
 
-const FaqItem = () => {
-  return <div>FaqItem</div>
+const FaqItem = ({item}) => {
+  return <div className='p-3 lg:p-5 rounded-[12px] border border-solid border-[#D9DCE2] mb-5 cursor-pointer'>
+    <div className='flex items-center justify-between gap-5'>
+
+    </div>
+  </div>
 }
 
 export default FaqItem;
