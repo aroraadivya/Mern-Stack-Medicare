4df56488da1a62e3a9b2be4b38f5fe2227e25627
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-13 01:05:05 +0530

    "Added Quick Links section to Footer component"

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -78,7 +78,7 @@ const Footer = () => {
     return <footer className='pb-16 pt-10'>
         <div className='container'>
             <div className='flex justify-between flex-col md:flex-row flex-wrap gap-[30px]'>
-                
+
                 <div>
                     <img src={logo} alt='' />
                     <p className='text-[16px] leading-7 font-[400] text-textColor mt-4'>Copyright {year} developed by Divya Arora.</p>
@@ -95,6 +95,16 @@ const Footer = () => {
                     <ul>
                         {quickLinks01.map((item,index)=> <li key={index} className='mb-4'> <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>
                         {item.display}
+                        </Link></li>)}
+                    </ul>
+                </div>
+
+                <div>
+                    <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>Quick Links</h2>
+
+                    <ul>
+                        {quickLinks01.map((item, index) => <li key={index} className='mb-4'> <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>
+                            {item.display}
                         </Link></li>)}
                     </ul>
                 </div>
