8a72eec5ec677796f5c9be03d71849c7b99d4fd3
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 14:40:06 +0530

    Refactor FaqItem component layout for improved readability

--- frontend/src/components/Faq/FaqItem.jsx
+++ frontend/src/components/Faq/FaqItem.jsx
@@ -5,15 +5,17 @@ import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
 
 const FaqItem = ({item}) => {
     return (
-  <div className='p-3 lg:p-5 rounded-[12px] border border-solid border-[#D9DCE2] mb-5 cursor-pointer'>
-    <div className='flex items-center justify-between gap-5'>
-        <h4 className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor'>
-            {item.question}
-        </h4>
-
-        <div className='w-7 h-7 lg:w-8 lg:h-8 border border-solid border-[#141F21] rounded flex items-center justify-center'>{isOpen ? <AiOutlineMinus/> : <AiOutlinePlus/>}</div>
+    <div className='p-3 lg:p-5 rounded-[12px] border border-solid border-[#D9DCE2] mb-5 cursor-pointer'>
+        <div className='flex items-center justify-between gap-5'>
+            <h4 className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor'>
+                {item.question}
+            </h4>
+            
+            <div className='w-7 h-7 lg:w-8 lg:h-8 border border-solid border-[#141F21] rounded flex items-center justify-center'>
+                {isOpen ? <AiOutlineMinus/> : <AiOutlinePlus/>}
+            </div>
+        </div>
     </div>
-  </div>
   );
 };
 
