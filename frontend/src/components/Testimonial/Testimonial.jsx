9a2edc4b4e2da73827578203618c7e2a7a5c60fb
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 17:58:21 +0530

    Implement responsive Swiper component in Testimonial section

--- frontend/src/components/Testimonial/Testimonial.jsx
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -9,7 +9,21 @@ import { HiStar } from 'react-icons/hi';
 const Testimonial = () => {
   return (
     <div className='mt-[30px] lg:mt-[55px]'>
-        <swiper>
+        <swiper modules={[Pagination]} spaceBetween={30} slidesPerView={1} pagination={{clickable:true}}
+        breakpoints={{
+            640: {
+                slidesPerView: 1,
+                spaceBetween: 0,
+              },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+              },
+            1024: {
+                slidesPerView: 3,
+                spaceBetween: 30,
+              },
+        }}>
 
         </swiper>
     </div>
