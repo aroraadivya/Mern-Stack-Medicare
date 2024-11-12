355dfa9145c259e1908487424e526e35a2303b4d
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 18:20:51 +0530

    Fix formatting in Testimonial component by removing unnecessary whitespace

--- frontend/src/components/Testimonial/Testimonial.jsx
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -34,7 +34,7 @@ const Testimonial = () => {
                             </h4>
                             <div className='flex items-center gap-[2px]'>
                                 <HiStar className='text-yellowColor w-[18px] h-5'/>
-                                
+                                  <HiStar className='text-yellowColor w-[18px] h-5' />   
                             </div>
                         </div>
                     </div>
