4194b66d843b0ed238019258289f20112e1ab9f0
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 18:07:40 +0530

    Add patient testimonial SwiperSlide to Testimonial component

--- frontend/src/components/Testimonial/Testimonial.jsx
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -24,6 +24,18 @@ const Testimonial = () => {
                 spaceBetween: 30,
               },
         }}>
+            <SwiperSlide>
+                <div className='py-[30px] px-5 rounded-3'>
+                      <div className='flex items-center gap-[13px]'>
+                          <img src={patientAvatar} alt='' />
+                          <div>
+                              <h4 className='text-[18px] leading-[30px] font-semibold text-headingColor'>
+                                  Muhbur Rahman
+                              </h4>
+                          </div>
+                      </div>
+                  </div>
+              </SwiperSlide>
         </Swiper>    
     </div>
   )
