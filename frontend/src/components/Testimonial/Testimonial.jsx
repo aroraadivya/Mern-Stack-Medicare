7a0d22a77237a8cc9644773d36946070604f7aaf
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 17:50:49 +0530

    "Added HiStar icon from react-icons/hi to Testimonial component"

--- frontend/src/components/Testimonial/Testimonial.jsx
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -4,6 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/pagination';
 import patientAvatar from '../../assets/images/patient-avatar.png';
+import { HiStar } from 'react-icons/hi';
 
 const Testimonial = () => {
   return (
