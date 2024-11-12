cbff19fc2f8dbd60fde50997d978b9c9d873aef0
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 20:59:06 +0530

    Add social media icon imports to Footer component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../../assets/images/logo.png';
 import { RiLinkedinFill } from 'react-icons/ri';
+import { AiFillYoutube, AiFillGithub, AiOutlineInstagram } from 'react-icons/ai';
 
 const Footer = () => {
     return (
