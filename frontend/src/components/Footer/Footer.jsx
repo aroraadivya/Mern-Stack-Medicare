024b6d2abf30661b0b4a3650e12ff28e883002ad
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-13 00:48:19 +0530

    "Updated Footer component: added 'gap-3 mt-4' classes to div element and removed content from Link component"

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -82,7 +82,7 @@ const Footer = () => {
                     <img src={logo} alt='' />
                     <p className='text-[16px] leading-7 font-[400] text-textColor mt-4'>Copyright {year} developed by Divya Arora.</p>
 
-                    <div className='flex items-center'>
+                    <div className='flex items-center gap-3 mt-4'>
                         {socialLinks.map((link, index)=> <Link></Link>)}
                     </div>
                 </div>
