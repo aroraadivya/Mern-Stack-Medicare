da8ce15561f83b0a897f8f7c2e83502260bd6ca3
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 21:25:42 +0530

    Add GitHub link to social links in Footer component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const socialLinks = [
         icon: <AiFillYoutube className='group-hover:text-white w-4 h-5'/>
     },
     {
-
+        path: 'https://github.com/'
     },
 ];
 const Footer = () => {
