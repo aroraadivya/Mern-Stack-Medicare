33388e4d88661b40db4f0fb029f4b85c3930dc08
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-13 00:47:28 +0530

    Add social links mapping in Footer component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -83,7 +83,7 @@ const Footer = () => {
                     <p className='text-[16px] leading-7 font-[400] text-textColor mt-4'>Copyright {year} developed by Divya Arora.</p>
 
                     <div>
-                        
+                        {socialLinks.map((link, index)=> <Link></Link>)}
                     </div>
                 </div>
             </div>
