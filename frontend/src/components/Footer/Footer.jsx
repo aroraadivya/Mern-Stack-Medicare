b6b204bbca3617a38610d85268409b5551324bb0
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 21:01:06 +0530

    Add placeholder for social links in Footer component

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -4,6 +4,9 @@ import logo from '../../assets/images/logo.png';
 import { RiLinkedinFill } from 'react-icons/ri';
 import { AiFillYoutube, AiFillGithub, AiOutlineInstagram } from 'react-icons/ai';
 
+const socialLinks = [
+    
+];
 const Footer = () => {
     return (
         <div>Footer</div>
