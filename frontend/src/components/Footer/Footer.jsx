220bb552f7fee51bf11e2df8abff0fd01a334205
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-13 00:46:55 +0530

    Add empty div to Footer component for layout adjustment

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -82,7 +82,9 @@ const Footer = () => {
                     <img src={logo} alt='' />
                     <p className='text-[16px] leading-7 font-[400] text-textColor mt-4'>Copyright {year} developed by Divya Arora.</p>
 
-                    
+                    <div>
+                        
+                    </div>
                 </div>
             </div>
         </div>
