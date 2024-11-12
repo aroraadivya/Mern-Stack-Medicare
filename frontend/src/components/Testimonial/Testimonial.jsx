c3bed3e2891f83965a39db3a2104a6f8881d94c4
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 15:00:22 +0530

    Fix formatting by adding missing semicolons in Testimonial component

--- frontend/src/components/Testimonial/Testimonial.jsx
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 const Testimonial = () => {
   return (
@@ -8,4 +8,4 @@ const Testimonial = () => {
   )
 }
 
-export default Testimonial
+export default Testimonial;
