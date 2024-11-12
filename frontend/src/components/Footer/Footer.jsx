70168dca81896fedb5d7b578b77a2dd8b147a120
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-13 01:03:15 +0530

    Update Footer component: style Quick Links for improved readability

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -92,7 +92,7 @@ const Footer = () => {
                     <h2 className='text-[20px] leading-[30px] font-[700] mb-6 text-headingColor'>Quick Links</h2>
 
                     <ul>
-                        {quickLinks01.map((item,index)=> <li key={index} className='mb-4'> <Link to={item.path}>
+                        {quickLinks01.map((item,index)=> <li key={index} className='mb-4'> <Link to={item.path} className='text-[16px] leading-7 font-[400] text-textColor'>
                         {item.display}
                         </Link></li>)}
                     </ul>
