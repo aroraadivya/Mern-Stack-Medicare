fdfdc0062a23dfad31fd3ad6b4fb5b2606dc92bd
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-12 14:26:05 +0530

    Refactor FaqItem component to improve JSX formatting and structure

--- frontend/src/components/Faq/FaqItem.jsx
+++ frontend/src/components/Faq/FaqItem.jsx
@@ -3,13 +3,15 @@ import FaqList from './FaqList';
 import { useState } from 'react';
 
 const FaqItem = ({item}) => {
-  return <div className='p-3 lg:p-5 rounded-[12px] border border-solid border-[#D9DCE2] mb-5 cursor-pointer'>
+  return (
+  <div className='p-3 lg:p-5 rounded-[12px] border border-solid border-[#D9DCE2] mb-5 cursor-pointer'>
     <div className='flex items-center justify-between gap-5'>
         <h4 className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor'>
             {item.question}
         </h4>
     </div>
   </div>
-}
+  );
+};
 
 export default FaqItem;
