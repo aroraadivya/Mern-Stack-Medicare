fa6fbafd42974835dc76c9281c3d7b52efab5d3e
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-13 13:34:59 +0530

    "Remove Services component content and fix syntax error"

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const Services = () => {
-    return (
-        <div>
-            <h1>Services</h1>
-        </div>
-    );
+    return
 };  
 
 export default Services;
