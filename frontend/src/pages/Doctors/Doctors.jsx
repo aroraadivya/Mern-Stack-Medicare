54d121d70fdf9cbb07ae2f0eedd13562457a8b2b
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-13 13:44:53 +0530

    Import doctors data in Doctors component

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import DoctorCard from './../../components/Doctors/DoctorCard';
+import { doctors } from './../../assets/data/doctors';
 
 const Doctors = () => {
     return (
