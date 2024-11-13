0279ae704334b126951e6d1b3bc2b10aac7b7b27
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-13 13:40:40 +0530

    Remove `mt-[30px]` from grid container styles in Services component.

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -5,7 +5,7 @@ import ServiceCard from './../components/Services/ServiceCard';
 const Services = () => {
     return <section>
         <div className='container'>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px]'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px]'>
                 {services.map((item, index) => <ServiceCard item={item} index={index} key={index} />)}
             </div>
         </div>
