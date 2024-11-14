9f6d79b1dd03a801e9052a295dca89d5a6a93dfd
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-14 23:58:54 +0530

    Remove unnecessary div wrapper in Doctors component for cleaner structure

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -6,15 +6,15 @@ const Doctors = () => {
     return (
         <>
             <section>
-                <div className='bg-[#fff9ea]'>
                     <div className='container text-center'>
                         <h2 className='heading'>Find a Doctor</h2>
 
                         <div className='max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between'>
                             <input type='search' className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor' placeholder='Search for a doctor' />
+
+
                         </div>
                     </div>
-                </div>
             </section>
         </>
     );
