fee9f64f4b357287ab3004b5b5c24db41c126252
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-15 00:02:18 +0530

    Style search button in Doctors component for improved appearance

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -12,7 +12,7 @@ const Doctors = () => {
                         <div className='max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between'>
                             <input type='search' className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor' placeholder='Search for a doctor' />
 
-                            <button>Search</button>
+                            <button className='btn mt-0 rounded-[0px] rounded-r-md'>Search</button>
                         </div>
                     </div>
             </section>
