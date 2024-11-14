98c9e87d85e40f0eebab45a377700fe46a0bfa3d
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-14 23:56:21 +0530

    Fix formatting of search input element in Doctors component

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -11,7 +11,7 @@ const Doctors = () => {
                         <h2 className='heading'>Find a Doctor</h2>
 
                         <div className='max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between'>
-                            <input type='search' className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor ' placeholder='Search for a doctor' />
+                            <input type='search' className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor' placeholder='Search for a doctor' />
                         </div>
                     </div>
                 </div>
