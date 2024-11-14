370777eb7615cd490e914eda57c1c9c19a3b68e4
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-15 00:04:40 +0530

    Refactor Doctors component layout for improved structure and readability

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -6,15 +6,15 @@ const Doctors = () => {
     return (
         <>
             <section className='bg-[#fff9ea]'>
-                    <div className='container text-center'>
-                        <h2 className='heading'>Find a Doctor</h2>
+                <div className='container text-center'>
+                    <h2 className='heading'>Find a Doctor</h2>
 
-                        <div className='max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between'>
-                            <input type='search' className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor' placeholder='Search for a doctor' />
+                    <div className='max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between'>
+                        <input type='search' className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor' placeholder='Search for a doctor' />
 
-                            <button className='btn mt-0 rounded-[0px] rounded-r-md'>Search</button>
-                        </div>
+                        <button className='btn mt-0 rounded-[0px] rounded-r-md'>Search</button>
                     </div>
+                </div>
             </section>
 
             <section>
