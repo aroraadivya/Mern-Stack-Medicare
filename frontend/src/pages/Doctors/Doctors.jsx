9b62b89fb347315e3ce71b8b0143b181bacffa47
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-14 23:42:20 +0530

    Add placeholder text to search input field in Doctors component

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -11,7 +11,7 @@ const Doctors = () => {
                         <h2 className='heading'>Find a Doctor</h2>
 
                         <div className='max-w-[570px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between'>
-                            <imput type='search' className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor '/>
+                            <imput type='search' className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor ' placeholder='Search for a doctor' />
                         </div>
                     </div>
                 </div>
