f207f7ee74672d37db9e232b47ac7bb240d97cf4
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-15 00:00:23 +0530

    Add search button to Doctors component for enhanced functionality

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -11,6 +11,8 @@ const Doctors = () => {
 
                         <div className='max-w-[570px] mt-[30px] mx-auto bg-[#0066ff2c] rounded-md flex items-center justify-between'>
                             <input type='search' className='py-4 pl-4 pr-2 bg-transparent w-full focus:outline-none cursor-pointer placeholder:text-textColor' placeholder='Search for a doctor' />
+
+                            <button>Search</button>
                         </div>
                     </div>
             </section>
