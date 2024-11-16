cc861d5d296975b41649452440efee45d5130254
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-16 22:16:12 +0530

    Added empty div element inside max-w-[1170] container in DoctorsDetails component.

--- frontend/src/pages/Doctors/DoctorsDetails.jsx
+++ frontend/src/pages/Doctors/DoctorsDetails.jsx
@@ -3,7 +3,9 @@ import React from "react";
 const DoctorsDetails = () => {
     return <section>
         <div className="max-w-[1170] px-5 mx-auto">
-
+            <div>
+                
+            </div>
         </div>
     </section>
 };
