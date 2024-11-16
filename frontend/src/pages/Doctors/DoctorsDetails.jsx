f63fda21d763b8a7ee526c5b3c48a8afa1bccda3
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-16 22:30:40 +0530

    Refactor DoctorsDetails component layout and add styling for "Surgeon" label

--- frontend/src/pages/Doctors/DoctorsDetails.jsx
+++ frontend/src/pages/Doctors/DoctorsDetails.jsx
@@ -3,7 +3,29 @@ import doctorImg from '../../assets/images/doctor-img02.png';
 
 const DoctorsDetails = () => {
     return (
-        
+        <section>
+            <div className="max-w-[1170] px-5 mx-auto">
+                <div className="grid md:grid-cols-3 gap-[50px]">
+
+                    <div className="md:col-span-2">
+                        <div className="flex items-center gap-5">
+                            <figure className="max-w-[200px] max-h-[200px]">
+                                <img src={doctorImg} alt="" className="w-full" />
+                            </figure>
+
+                            <div>
+                                <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px]">Surgeon</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div>
+
+                    </div>
+
+                </div>
+            </div>
+        </section>
     );
 };
 
