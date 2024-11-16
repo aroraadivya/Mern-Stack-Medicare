66c9051ca8cc1e5c419e2273253192e1aa1c1bf1
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-16 22:29:59 +0530

    Fix formatting issue in DoctorsDetails component return statement

--- frontend/src/pages/Doctors/DoctorsDetails.jsx
+++ frontend/src/pages/Doctors/DoctorsDetails.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import doctorImg from '../../assets/images/doctor-img02.png';
 
 const DoctorsDetails = () => {
-    return 
+    return (
+        
+    );
 };
 
 export default DoctorsDetails;
