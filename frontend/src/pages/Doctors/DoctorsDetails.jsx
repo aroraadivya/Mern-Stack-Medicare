7c9430dd5534599141435efe7897d211eea9c642
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-16 22:18:26 +0530

    Added empty div element to grid in DoctorsDetails component.

--- frontend/src/pages/Doctors/DoctorsDetails.jsx
+++ frontend/src/pages/Doctors/DoctorsDetails.jsx
@@ -5,6 +5,10 @@ const DoctorsDetails = () => {
         <div className="max-w-[1170] px-5 mx-auto">
             <div className="grid md:grid-cols-3 gap-[50px]">
 
+                <div>
+                    
+                </div>
+
             </div>
         </div>
     </section>
