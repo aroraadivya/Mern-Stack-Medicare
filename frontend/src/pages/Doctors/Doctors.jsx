cc34269ba1a83fd3dd53285cb831c300fc4fca7a
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-16 22:09:38 +0530

    "Updated grid-cols class in Doctors component to include lg:grid-cols-4"

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -19,7 +19,7 @@ const Doctors = () => {
 
             <section>
                 <div className='container'>
-                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
+                    <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 lg:gap-[30px] mt-[30px] lg:mt-[55px]'>
                         {doctors.map((doctor) => <DoctorCard key={doctor.id} doctor={doctor} />)}
                     </div>
                 </div>
