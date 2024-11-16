35c551c1e4995b821cdd063fdb4d00afbeb66d88
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-16 22:18:01 +0530

    Update DoctorsDetails component to add grid layout to inner div

--- frontend/src/pages/Doctors/DoctorsDetails.jsx
+++ frontend/src/pages/Doctors/DoctorsDetails.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const DoctorsDetails = () => {
     return <section>
         <div className="max-w-[1170] px-5 mx-auto">
-            <div>
-                
+            <div className="grid md:grid-cols-3 gap-[50px]">
+
             </div>
         </div>
     </section>
