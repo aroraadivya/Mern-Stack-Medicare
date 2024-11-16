371044c23dfcdd6d46b2076bbbc28c2b56fadd65
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-16 22:31:11 +0530

    Fix CSS class width in DoctorsDetails component

--- frontend/src/pages/Doctors/DoctorsDetails.jsx
+++ frontend/src/pages/Doctors/DoctorsDetails.jsx
@@ -4,7 +4,7 @@ import doctorImg from '../../assets/images/doctor-img02.png';
 const DoctorsDetails = () => {
     return (
         <section>
-            <div className="max-w-[1170] px-5 mx-auto">
+            <div className="max-w-[1170px] px-5 mx-auto">
                 <div className="grid md:grid-cols-3 gap-[50px]">
 
                     <div className="md:col-span-2">
