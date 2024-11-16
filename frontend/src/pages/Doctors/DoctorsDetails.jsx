84136ef347b263c9ede4050e1918edce93f5dfec
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-16 22:34:49 +0530

    Add empty heading element in DoctorsDetails component

--- frontend/src/pages/Doctors/DoctorsDetails.jsx
+++ frontend/src/pages/Doctors/DoctorsDetails.jsx
@@ -15,6 +15,7 @@ const DoctorsDetails = () => {
 
                             <div>
                                 <span className="bg-[#CCF0F3] text-irisBlueColor py-1 px-6 lg:py-2 lg:px-6 text-[12px] leading-4 lg:text-[16px] lg:leading-7 font-semibold rounded">Surgeon</span>
+                                <h3></h3>
                             </div>
                         </div>
                     </div>
