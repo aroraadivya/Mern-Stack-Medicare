abc194775c8b049d6205dbf73e11c36295927a31
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-16 22:11:45 +0530

    Add testimonial section to Doctors component for patient feedback

--- frontend/src/pages/Doctors/Doctors.jsx
+++ frontend/src/pages/Doctors/Doctors.jsx
@@ -24,6 +24,15 @@ const Doctors = () => {
                     </div>
                 </div>
             </section>
+
+            <div className="container">
+                <div className='xl:w-[470px] mx-auto'>
+                    <h2 className='heading text-center'>What our patient Say</h2>
+                    <p className='text__para text-center'>World-class care for everyone. Our health system offers unmatched, expert health care.</p>
+                </div>
+
+                <Testimonial />
+            </div>
         </>
     );
 };
