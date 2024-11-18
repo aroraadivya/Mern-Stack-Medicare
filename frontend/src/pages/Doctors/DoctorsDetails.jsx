60cbbc6580ad712b4f0050844c03ffccc446dca5
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-18 13:59:11 +0530

    Add DoctorAbout component to DoctorsDetails for enhanced doctor information display

--- frontend/src/pages/Doctors/DoctorsDetails.jsx
+++ frontend/src/pages/Doctors/DoctorsDetails.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import doctorImg from '../../assets/images/doctor-img02.png';
 import starIcon from '../../assets/images/Star.png';
 import { useState } from "react";
+import DoctorAbout from "./DoctorAbout";
 
 const DoctorsDetails = () => {
 
@@ -45,7 +46,9 @@ const DoctorsDetails = () => {
                         </div>
 
                         <div className="mt-[50px]">
-
+                            {
+                                tab==='about' && <DoctorAbout />
+                            }
                         </div>
                     </div>
 
