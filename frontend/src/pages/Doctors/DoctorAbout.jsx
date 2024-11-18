83115ce40eed1ca7a230d6ff894573029e308870
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-18 14:06:30 +0530

    Add doctor's name to heading in DoctorAbout component for personalization

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -6,6 +6,7 @@ const DoctorAbout = () => {
         <div>
             <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold flex items-center gap-2'>
                 About of
+                <span>Muhbir Rahman</span>
             </h3>
         </div>
     </div>
