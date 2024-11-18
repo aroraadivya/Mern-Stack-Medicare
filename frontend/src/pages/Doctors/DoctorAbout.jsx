1320cd7549516cee9bdd1c0a6b7c2cee9758b603
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-18 14:22:47 +0530

    Update degree information in Education list item of DoctorAbout component

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -22,7 +22,7 @@ const DoctorAbout = () => {
 
                         <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in Surgeon</p>
                     </div>
-                      <p className='text-[16px] leading-5 font-medium text-textColor'>PHD in Surgeon</p>
+                      <p className='text-[16px] leading-5 font-medium text-textColor'>New Apollo Hospital, New York.</p>
                 </li>
             </ul>
         </div>
