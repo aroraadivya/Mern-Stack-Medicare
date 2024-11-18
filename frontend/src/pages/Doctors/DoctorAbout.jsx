8a2cffcac7fe7a59985f49ce2033ab42a10708d5
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-18 14:30:47 +0530

    Update DoctorAbout component to use formateDate utility for date formatting

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -19,7 +19,7 @@ const DoctorAbout = () => {
             <ul className='pt-4 md:p-5'>
                 <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
                     <div>
-                        <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>23 June, 2008</span>
+                          <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>{formateDate("09-13-2014")}</span>
 
                         <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in Surgeon</p>
                     </div>
@@ -28,7 +28,7 @@ const DoctorAbout = () => {
 
                   <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
                       <div>
-                          <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>{formateDate("12-04-2010")}</span>
+                          <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>{formateDate("07-04-2010")}</span>
 
                           <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in Surgeon</p>
                       </div>
