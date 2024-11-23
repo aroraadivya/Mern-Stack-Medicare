6e6a089abc7db75a42bde4e12bdcc8cfc34ca3b1
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 01:26:31 +0530

    Add state management for rating in FeedbackForm component

--- frontend/src/pages/Doctors/FeedbackForm.jsx
+++ frontend/src/pages/Doctors/FeedbackForm.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { AiFillStar } from 'react-icons/ai';
 
 const FeedbackForm = () => {
+
+    const [rating,setRating] = useState(0);
   return (
     <form action=''>
         <div>
