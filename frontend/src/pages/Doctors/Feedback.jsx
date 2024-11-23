c3ab108315c8992b95fb415c2dbcfc725f6481e3
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-23 23:33:33 +0530

    Refactor Feedback component layout to enhance structure and spacing

--- frontend/src/pages/Doctors/Feedback.jsx
+++ frontend/src/pages/Doctors/Feedback.jsx
@@ -7,8 +7,8 @@ const Feedback = () => {
         <h4 className='text-[20px] leading-[30px] font-bold text-headingColor mb-[30px]'>All reviews(272)</h4>
 
         <div className='flex justify-between gap-10 mb-[30px]'>
-          <div>
-            
+          <div className='flex gap-3'>
+
           </div>
         </div>
       </div>
