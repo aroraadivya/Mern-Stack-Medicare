d1a93daa8e60e9ca4b86d62c347fe36a260a0522
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-23 23:30:10 +0530

    Enhance styling of review count header in Feedback component

--- frontend/src/pages/Doctors/Feedback.jsx
+++ frontend/src/pages/Doctors/Feedback.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Feedback = () => {
   return (
     <div className='mb-[50px]'>
-      <h4>All reviews(272)</h4>
+      <h4 className='text-[20px] leading-[30px] font-bold text-headingColor mb-[30px]'>All reviews(272)</h4>
     </div>
   );
 };
