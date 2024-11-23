61c343277aafaf62b6801405cafaa917271a15fc
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-23 23:31:13 +0530

    Add review count header and margin to Feedback component

--- frontend/src/pages/Doctors/Feedback.jsx
+++ frontend/src/pages/Doctors/Feedback.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Feedback = () => {
   return (
     <div>
-      
+      <div className='mb-[50px]'>
+        <h4 className='text-[20px] leading-[30px] font-bold text-headingColor mb-[30px]'>All reviews(272)</h4>
+      </div>
     </div>
   );
 };
