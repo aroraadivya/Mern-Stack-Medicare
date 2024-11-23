612edd6f01ec4fe862a9d943af9599bcea2dcbea
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 01:23:26 +0530

    Enhance heading styling in FeedbackForm component for better visibility

--- frontend/src/pages/Doctors/FeedbackForm.jsx
+++ frontend/src/pages/Doctors/FeedbackForm.jsx
@@ -4,7 +4,7 @@ const FeedbackForm = () => {
   return (
     <form action=''>
         <div>
-            <h3>
+            <h3 className='text-headingColor text-[16px] leading-6 font-semibold mb-4'>
                 How would you like overall experience?*
             </h3>
         </div>
