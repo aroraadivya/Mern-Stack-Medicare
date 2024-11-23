5b0fecec387a3bca203de44a9cd8436ba2ce714a
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 01:10:57 +0530

    Import useState hook in Feedback component for state management

--- frontend/src/pages/Doctors/Feedback.jsx
+++ frontend/src/pages/Doctors/Feedback.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import avatar from '../../assets/images/avatar-icon.png';
 import { formateDate } from '../../utils/formateDate';
 import { AiFillStar } from 'react-icons/ai';
+import { useState } from 'react';
 
 const Feedback = () => {
 
