50b3ef4d34d36bd80b958e1107730e1c218b531d
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-23 23:32:24 +0530

    Update Feedback component layout with flexbox for improved structure

--- frontend/src/pages/Doctors/Feedback.jsx
+++ frontend/src/pages/Doctors/Feedback.jsx
@@ -6,8 +6,8 @@ const Feedback = () => {
       <div className='mb-[50px]'>
         <h4 className='text-[20px] leading-[30px] font-bold text-headingColor mb-[30px]'>All reviews(272)</h4>
 
-        <div>
-          
+        <div className='flex justify-between gap-10 mb-[30px]'>
+
         </div>
       </div>
     </div>
