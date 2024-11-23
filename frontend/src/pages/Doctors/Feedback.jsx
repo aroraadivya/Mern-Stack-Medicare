877881e8b1aadcd96d9350e6ea57f99e8724b8c4
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-23 23:30:54 +0530

    Remove unnecessary wrapper and header from Feedback component

--- frontend/src/pages/Doctors/Feedback.jsx
+++ frontend/src/pages/Doctors/Feedback.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 
 const Feedback = () => {
   return (
-    <div className='mb-[50px]'>
-      <h4 className='text-[20px] leading-[30px] font-bold text-headingColor mb-[30px]'>All reviews(272)</h4>
-    </div>
+    
   );
 };
 
