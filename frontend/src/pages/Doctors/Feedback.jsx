6be5c83afd47ef9d499e41d17374f068431a1813
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 01:10:20 +0530

    Add state to manage feedback form visibility in Feedback component

--- frontend/src/pages/Doctors/Feedback.jsx
+++ frontend/src/pages/Doctors/Feedback.jsx
@@ -4,6 +4,8 @@ import { formateDate } from '../../utils/formateDate';
 import { AiFillStar } from 'react-icons/ai';
 
 const Feedback = () => {
+
+  const [showFeedbackForm, setShowFeedbackForm] = useState(false);
   return (
     <div>
       <div className='mb-[50px]'>
