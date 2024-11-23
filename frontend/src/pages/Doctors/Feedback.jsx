32d6b7a7c575e5e726b1f0691af71a56b97b7083
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-23 23:32:37 +0530

    Add empty wrapper div for future content in Feedback component

--- frontend/src/pages/Doctors/Feedback.jsx
+++ frontend/src/pages/Doctors/Feedback.jsx
@@ -7,7 +7,9 @@ const Feedback = () => {
         <h4 className='text-[20px] leading-[30px] font-bold text-headingColor mb-[30px]'>All reviews(272)</h4>
 
         <div className='flex justify-between gap-10 mb-[30px]'>
-
+          <div>
+            
+          </div>
         </div>
       </div>
     </div>
