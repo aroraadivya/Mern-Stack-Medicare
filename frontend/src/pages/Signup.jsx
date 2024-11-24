678b2a5bdbb4915135760fc9d2c2c538e473657c
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 14:30:27 +0530

    Add default 'Select' option to gender selection in Signup form

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -59,6 +59,7 @@ const Signup = () => {
                                 <label className='text-headingColor font-bold text-[16px] leading-7' >
                                     Are you a:
                                     <select name='Gender' className='text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none'>
+                                        <option value="">Select</option>
                                         <option value="Male">Male</option>
                                         <option value="Female">Female</option>
                                         <option value="Transgender">Transgender</option>
