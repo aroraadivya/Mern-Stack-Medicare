08cc8cce8c8c8bbfcf70cc41a7b3dc860962a729
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 13:11:35 +0530

    Refactor Signup component layout for improved responsiveness

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Signup = () => {
     return (
         <section className='px-5 xl:px-0'>
-            <div>
-                
+            <div className='max-w-[1170px] mx-auto'>
+
             </div>
         </section>
     );
