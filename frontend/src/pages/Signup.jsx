c740f1e2bdb617ec7a78d68d68670d471df8fa63
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 13:14:31 +0530

    Enhance Signup component layout by adding a styled image box for larger screens

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -6,8 +6,8 @@ const Signup = () => {
             <div className='max-w-[1170px] mx-auto'>
                 <div className='grid grid-cols-1 lg:grid-cols-2'>
                     {/* image box */}
-                    <div>
-                        
+                    <div className='hidden lg:block bg-primaryColor rounded-l-lg'>
+
                     </div>
                 </div>
             </div>
