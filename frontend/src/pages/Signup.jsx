44db5109fe758d69dbf0c8d6a5250bacf0bbe4b1
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 13:34:45 +0530

    Add Full Name input field to Signup form

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -37,6 +37,15 @@ const Signup = () => {
                                     value=''
                                     className='w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-primaryColor focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer' required />
                             </div>
+
+                            <div className='mb-5'>
+                                <input
+                                    type='text'
+                                    placeholder='Full Name'
+                                    name='name'
+                                    value=''
+                                    className='w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-primaryColor focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer' required />
+                            </div>
                         </form>
                     </div>
                 </div>
