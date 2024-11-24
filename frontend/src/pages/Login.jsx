f660d39922bb12df7d0161691e203fa2eebbff65
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 12:36:51 +0530

    Add margin to div in Login component for improved layout

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -44,8 +44,8 @@ const Login = () => {
                                 className='w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-primaryColor focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer' required />
                         </div>
 
-                        <div>
-                            
+                        <div className='mt-7'>
+
                         </div>
                     </div>
                 </form>
