22c1a43f8f06b3f85639995ff24c66912c642682
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 12:19:05 +0530

    Add styling to div in Login component for improved layout

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Login = () => {
     return (
         <section className='px-5 lg:px-0'>
-            <div>
-                
+            <div className='w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10'>
+
             </div>
         </section>
     );
