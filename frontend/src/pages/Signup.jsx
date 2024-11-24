52bb044de5f5793664d7904dd5b8cd75bfaa83c5
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 13:35:13 +0530

    Change Full Name input field to Email input in Signup form

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -31,9 +31,9 @@ const Signup = () => {
 
                             <div className='mb-5'>
                                 <input
-                                    type='text'
-                                    placeholder='Full Name'
-                                    name='name'
+                                    type='email'
+                                    placeholder='Enter your email address'
+                                    name='email'
                                     value=''
                                     className='w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-primaryColor focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer' required />
                             </div>
