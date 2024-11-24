7f8243e7ad8142eef1abc78840ca22da20ea522f
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 13:11:10 +0530

    Add empty div to Signup component for future content

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Signup = () => {
     return (
         <section className='px-5 xl:px-0'>
-
+            <div>
+                
+            </div>
         </section>
     );
 };
