72b44f0212ca9724aee0e780210cce6434d43e9e
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 11:43:19 +0530

    Enhance ticket price display styling in SidePanel component

--- frontend/src/pages/Doctors/SidePanel.jsx
+++ frontend/src/pages/Doctors/SidePanel.jsx
@@ -5,7 +5,7 @@ const SidePanel = () => {
     <div className='shadow-panelShadow p-3 lg:p-5 rounded-md'>
         <div className='flex items-center justify-between'>
             <p className='text__para mt-0 font-semibold'>Ticket Price</p>
-            <span>
+            <span className='text-[16px] leading-7 lg:text-[22px] lg:leading-8 text-headingColor font-bold'>
                 500 BDT
             </span>
         </div>
