447d1ad4f119c7d05771e780259ce856d4c32246
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 12:19:35 +0530

    Add welcome heading to Login component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,7 +4,9 @@ const Login = () => {
     return (
         <section className='px-5 lg:px-0'>
             <div className='w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10'>
-
+                <h3>
+                    Welcome
+                </h3>
             </div>
         </section>
     );
