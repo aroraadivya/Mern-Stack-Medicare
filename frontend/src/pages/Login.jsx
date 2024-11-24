31464bf1eff8a97fa0d2fc3de537a4a38916a21f
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 12:33:49 +0530

    Adjust font size of email input field in Login component

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
                         name='email'
                         value={FormData.email}
                         onChange={handleInputChange} 
-                        className='w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-primaryColor focus:border-b-primaryColor text-[22px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer' />
+                        className='w-full px-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-primaryColor focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer' />
                     </div>
                 </form>
             </div>
