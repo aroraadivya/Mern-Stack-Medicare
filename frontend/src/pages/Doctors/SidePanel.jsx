7404a03555b39e4a064355c30e2e1c53e011f579
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 11:41:05 +0530

    Add "Ticket Price" label to SidePanel component

--- frontend/src/pages/Doctors/SidePanel.jsx
+++ frontend/src/pages/Doctors/SidePanel.jsx
@@ -4,7 +4,7 @@ const SidePanel = () => {
   return (
     <div className='shadow-panelShadow p-3 lg:p-5 rounded-md'>
         <div className='flex items-center justify-between'>
-
+            <p>Ticket Price</p>
         </div>
     </div>
   );
