567d9017011d03a2f665e3a0b1a23a5a4e2705cf
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 13:11:49 +0530

    Add empty div to Signup component for future content

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -4,7 +4,9 @@ const Signup = () => {
     return (
         <section className='px-5 xl:px-0'>
             <div className='max-w-[1170px] mx-auto'>
-
+                <div>
+                    
+                </div>
             </div>
         </section>
     );
