e05d931853e2889dc98c0bfd26e35b4268dcff48
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 12:22:03 +0530

    Fix spacing in welcome message of Login component heading

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,9 +5,9 @@ const Login = () => {
         <section className='px-5 lg:px-0'>
             <div className='w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10'>
                 <h3 className='text-headingColor text-[22px] leading-9 font-bold mb-10'>
-                    Hello!<span className='text-primaryColor'>
+                    Hello! <span className='text-primaryColor'>
                         Welcome
-                    </span>Back
+                    </span> Back
                 </h3>
             </div>
         </section>
