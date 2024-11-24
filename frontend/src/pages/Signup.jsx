4b97009faadd11e05506ebbab9ea87d54983749c
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 14:21:12 +0530

    Refactor Signup form layout for improved structure and spacing

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -47,8 +47,8 @@ const Signup = () => {
                                     className='w-full pr-4 py-3 border-b border-solid border-[#0066ff61] focus:outline-none focus:border-primaryColor focus:border-b-primaryColor text-[16px] leading-7 text-headingColor placeholder:text-textColor rounded-md cursor-pointer' required />
                             </div>
 
-                            <div>
-                                
+                            <div className='mb-5 flex items-center justify-between'>
+
                             </div>
                         </form>
                     </div>
