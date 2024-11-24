00fe9ebaf56a87eaceb0fd87c5df513f9f6dba33
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 12:20:37 +0530

    Remove welcome text from Login component heading

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -4,8 +4,7 @@ const Login = () => {
     return (
         <section className='px-5 lg:px-0'>
             <div className='w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10'>
-                <h3>
-                    Welcome
+                <h3 >
                 </h3>
             </div>
         </section>
