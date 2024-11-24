3f1ff005cf1f15a1dc271136a7b00a46355e9750
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 12:17:46 +0530

    Add empty div to Login component for future content

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Login = () => {
     return (
         <section className='px-5 lg:px-0'>
-
+            <div>
+                
+            </div>
         </section>
     );
 };
