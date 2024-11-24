a0903477bf3049d20996da527338fff192814526
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 13:13:26 +0530

    Add comment for image box in Signup component layout

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -5,7 +5,7 @@ const Signup = () => {
         <section className='px-5 xl:px-0'>
             <div className='max-w-[1170px] mx-auto'>
                 <div className='grid grid-cols-1 lg:grid-cols-2'>
-
+                    {/* image box */}
                 </div>
             </div>
         </section>
