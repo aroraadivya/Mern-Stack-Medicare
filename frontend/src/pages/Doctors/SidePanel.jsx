ea9fca18531dabdfd76432c58d3c9d266ce329ab
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-24 11:40:31 +0530

    Add empty div structure to SidePanel component

--- frontend/src/pages/Doctors/SidePanel.jsx
+++ frontend/src/pages/Doctors/SidePanel.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const SidePanel = () => {
   return (
     <div className='shadow-panelShadow p-3 lg:p-5 rounded-md'>
-        
+        <div>
+            
+        </div>
     </div>
   );
 };
