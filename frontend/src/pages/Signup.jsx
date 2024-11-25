0e88c9aeb495e3b54ec5bbcd9c82f7f09f9aa0ae
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-25 20:05:18 +0530

    Add selectedFile state to Signup component for file handling

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 
 const Signup = () => {
 
+    const [selectedFile, setSelectedFile] = useState(null);
     const [FormData, setFormData] = useState({
         name:'',
         email: '',
