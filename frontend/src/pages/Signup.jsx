6111d8aef851c2781db3c0f2e3088ed6699071d6
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-25 20:19:05 +0530

    Bind role selection field to formData in Signup component

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -76,7 +76,7 @@ const Signup = () => {
                                 <label className='text-headingColor font-bold text-[16px] leading-7' >
                                     Are you a:
                                     <select name='role'
-                                    value={formData.name}
+                                    value={formData.role}
                                     onChange={handleInputChange}
                                     className='text-textColor font-semibold text-[15px] leading-7 px-4 py-3 focus:outline-none'>
                                         <option value="patient">Patient</option>
