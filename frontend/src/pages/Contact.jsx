c77e09d09663dee5c707d023b7df3c5e83603dd8
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-26 23:16:53 +0530

    Add subject input field to Contact form

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -32,6 +32,18 @@ const Contact = () => {
                             className='form__input mt-1'
                         />
                     </div>
+
+                    <div>
+                        <label htmlFor='subject' className='form__label'>
+                            Subject
+                        </label>
+                        <input
+                            type='text'
+                            id='subject'
+                            placeholder='Let us know how we can help you.'
+                            className='form__input mt-1'
+                        />
+                    </div>
                 </form>
             </div>
         </section>
