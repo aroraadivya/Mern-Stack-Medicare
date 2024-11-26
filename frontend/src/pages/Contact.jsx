bc17fb86fc5ebfdc53dd119031fd3b77ecb7c1ed
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-26 23:05:58 +0530

    Add empty div to Contact component

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Contact = () => {
     return (
         <section>
-            
+            <div>
+                
+            </div>
         </section>
     );
 };
