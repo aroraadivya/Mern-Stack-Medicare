5a98d69148c70d7d501a24f0469bdfd256cb40f4
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-26 23:10:56 +0530

    Add email input field to Contact form

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -6,6 +6,21 @@ const Contact = () => {
             <div className='px-4 mx-auto max-w-screen-md'>
                 <h2 className='heading text-center'>Contact Us</h2>
                 <p className='mb-8 lg:mb-16 font-light text-center text__para'>Got a technical issue? Want to send feedback about a beta feature? Let us know.</p>
+
+                <form action='#' className='space-y-8'>
+                    <div>
+                        <label htmlFor='email' className='form__label'>
+                            Your Email
+                        </label>
+                        <input
+                            type='email'
+                            id='email'
+                            name='email'
+                            placeholder='example@gmail.com'
+                            className='form__input mt-1'
+                        />
+                    </div>
+                </form>
             </div>
         </section>
     );
