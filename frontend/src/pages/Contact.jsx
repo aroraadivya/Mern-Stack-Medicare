914355331f4a15654d933653e6aa45517ab41a5e
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-26 23:08:33 +0530

    Update Contact component with heading and description

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -3,8 +3,9 @@ import React from 'react';
 const Contact = () => {
     return (
         <section>
-            <div>
-                
+            <div className='px-4 mx-auto max-w-screen-md'>
+                <h2 className='heading text-center'>Contact Us</h2>
+                <p className='mb-8 lg:mb-16 font-light text-center text__para'>Got a technical issue? Want to send feedback about a beta feature? Let us know.</p>
             </div>
         </section>
     );
