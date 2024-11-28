45d7ee4350a2429a2cffb973802735ea45be1676
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-11-28 14:43:36 +0530

    Implement user registration logic for patients and doctors with role-based handling

--- backend/Controllers/authController.js
+++ backend/Controllers/authController.js
@@ -1,8 +1,58 @@
+import User from '../Models/UserSchema.js';
+import Doctor from '../Models/DoctorSchema.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
 export const register = async (req, res)=> {
+    
+    const {email, password, name, role, photo, gender} = req.body
+
     try{
+        let user = null
+
+        if(role==='patient'){
+            user = User.findOne({email})
+        }
+        else if(role==='doctor'){
+            user = Doctor.findOne({email})
+        }
+
+        // check if user exists
+        if(user) {
+            return res.status(400).json({msg: 'User already exists'});
+        }
+
+        // hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        if(role==='patient'){
+            user = new User({
+                name,
+                email,
+                password: hashedPassword,
+                photo,
+                gender,
+                role
+            })
+        }
+
+        if(role==='doctor'){
+            user = new Doctor({
+                name,
+                email,
+                password: hashedPassword,
+                photo,
+                gender,
+                role
+            })
+        }
+
+        await user.save();
+        res.status(200).json({success:true, message:'User created successfully'});
 
     } catch (err){
-
+        res.status(500).json({success:false, message:'Internal server error'});
     }
 };
 
