9294ce73e7872bc45d2b8321bc7b3e119b14b458
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-03 14:29:24 +0530

    Add review route to main application file

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,8 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import authRoute from './Routes/auth.js';
 import userRoute from './Routes/user.js';
-import doctorRoute from './Routes/doctor.js';
+import doctorRoute from "./Routes/doctor.js";
+import reviewRoute from "./Routes/review.js";
 
 dotenv.config();
 
@@ -42,6 +43,7 @@ app.use(cors(crosOptions));
 app.use('/api/v1/auth', authRoute);
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/doctors", doctorRoute);
+app.use("/api/v1/reviews", reviewRoute);
 
 
 app.listen(port, () => {
