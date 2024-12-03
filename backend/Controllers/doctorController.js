e08293d7b1e67d4a1e7ed05c9719c3813110a5c5
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-03 11:27:50 +0530

    Enhance getAllDoctor function to support query filtering for approved doctors by name or specialization

--- backend/Controllers/doctorController.js
+++ backend/Controllers/doctorController.js
@@ -129,7 +129,22 @@ export const getSingleDoctor = async (req, res) => {
 
 export const getAllDoctor = async (req, res) => {
   try {
-    const doctors = await Doctor.find({}); // Renamed to `doctors` for consistency
+    const { query } = req.query;
+    let doctors;
+
+    if (query) {
+      doctors = await Doctor.find({
+        isApproved: "approved",
+        $or: [
+          { name: { $regex: query, $options: "i" } },
+          { specialization: { $regex: query, $options: "i" } },
+        ],
+      }).select("-password");
+    } else{
+        doctors = await Doctor.find({}).select(
+          "-password"
+        );
+    }
 
     res.status(200).json({
       success: true,
