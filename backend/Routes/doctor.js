09a5480f3c96b38bba48102d5fbb8d4a7d481833
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-03 14:06:13 +0530

    Implement authentication and role-based access control for doctor and user routes

--- backend/Routes/doctor.js
+++ backend/Routes/doctor.js
@@ -5,12 +5,13 @@ import {
   getAllDoctor,
   getSingleDoctor,
 } from "../Controllers/doctorController.js";
+import { authenticate, restrict } from "../auth/verifyToken.js";
 
 const router = express.Router();
 
 router.get("/:id", getSingleDoctor);
 router.get("/", getAllDoctor);
-router.put("/:id", updateDoctor);
-router.delete("/:id", deleteDoctor);
+router.put("/:id", authenticate, restrict(["doctor"]), updateDoctor);
+router.delete("/:id", authenticate, restrict(["doctor"]), deleteDoctor);
 
 export default router;
--- backend/Routes/user.js
+++ backend/Routes/user.js
@@ -6,9 +6,9 @@ import { authenticate, restrict } from "../auth/verifyToken.js";
 
 const router = express.Router();
 
-router.get('/:id', authenticate,restrict(['admin']), getSingleUser);
-router.get("/", getAllUser);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
+router.get('/:id', authenticate,restrict(['patient']), getSingleUser);
+router.get("/", authenticate, restrict(["admin"]), getAllUser);
+router.put("/:id", authenticate, restrict(["patient"]), updateUser);
+router.delete("/:id", authenticate, restrict(["patient"]), deleteUser);
 
 export default router;
