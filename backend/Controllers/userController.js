59e0b8a3ff099003b1b7dfd0756d36cc2c339c6f
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-10 22:17:07 +0530

    Add user profile retrieval and skeleton for appointment fetching in userController

--- backend/Controllers/userController.js
+++ backend/Controllers/userController.js
@@ -68,8 +68,30 @@ export const getUserProfile = async(req,res) => {
   const userId = req.userId;
 
   try {
+    const user = await User.findById(userId);
+
+    if(!user){
+      return res.status(404).json({success:false, message:'User not found'});
+    }
+
+    const {password, ...rest} = user._doc;
+
+    res.status(200).json({success:true, message:'Profile info is getting', data:rest});
 
   } catch (err) {
-
+    res.status(500).json({ success: false, message: "Something went wrong, cannot get" });
   }
 };
+
+export const getMyAppointments = async(req, res) => {
+  try{
+
+    // step -1:retrieve appointments from booking specific user
+
+    // step -2:extract doctor ids from appointment bookings
+
+    // step -3:retrieve doctors from doctor ids
+  } catch (err) {
+    
+  }
+}
