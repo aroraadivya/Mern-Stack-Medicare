d43753e1a0de2bc17726bae1bc6f6cb9ce76ad53
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-18 00:38:43 +0530

    Add handleLogout function and enhance MyAccount component with additional action buttons

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -12,9 +12,7 @@ const Myaccount = () => {
 
   return (
     <div className='max-w-[1170] px-5 mx-auto'>
-
       <div className='grid md:grid-cols-3 gap-10'>
-
         <div className='pb-[50px] px-[30px] rounded-md'>
           <div className='flex items-center justify-center'>
             <figure className='w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor'>
@@ -30,11 +28,21 @@ const Myaccount = () => {
             </div>
 
             <div className='mt-[50px] md:mt-[100px]'>
-              <button className='w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white'>Logout</button>
+              <button onClick={handleLogout} className='w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white'>Logout</button>
               <button className='w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white'>Delete Account</button>
             </div>
           </div>
 
+        </div>
+
+        <div className='md:col-span-2 md:px-[30px]'>
+          <div>
+            <div>
+              <button className='p-2 mr-5 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor'>Bookings</button>
+
+              <button className='py-2 px-5 rounded-md text-headingColor font-semibold text-[16px] leading-7 border border-solid border-primaryColor'>Profile Settings</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
