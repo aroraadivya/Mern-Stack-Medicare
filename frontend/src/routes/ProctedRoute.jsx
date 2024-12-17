6409ed51967e742de3e909f9f201d94c04e826ba
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-17 21:26:42 +0530

    Add ProtectedRoute component for route protection

---
+++ frontend/src/routes/ProctedRoute.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { authContext } from '../context/AuthContext';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const ProctedRoute = () => {
+  return (
+    <div>
+          ProctedRoute
+    </div>
+  )
+}
+
+export default ProctedRoute
