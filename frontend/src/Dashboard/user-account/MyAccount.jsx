dad3e1321cad6d883da3b3e462349f8bbcc6567e
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-18 00:04:50 +0530

    Enhance MyAccount component with user details and action buttons for logout and account deletion

--- frontend/src/Dashboard/user-account/MyAccount.jsx
+++ frontend/src/Dashboard/user-account/MyAccount.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import {useContext} from 'react';
 import userImg from '../../assets/images/doctor-img01.png';
 
 const Myaccount = () => {
@@ -12,6 +12,19 @@ const Myaccount = () => {
             <figure className='w-[100px] h-[100px] rounded-full border-2 border-solid border-primaryColor'>
               <img src={userImg} alt='' className='w-full h-full rounded-full' />
             </figure>
+          </div>
+
+          <div>
+            <div className='text-center mt-4'>
+              <h3 className='text-[18px] leading-[30px] text-headingColor font-bold'>Muhbir Rahman</h3>
+              <p className='text-textColor text-[15px] leading-6 font-medium'>example@gmail.com</p>
+              <p className='text-textColor text-[15px] leading-6 font-medium'>Blood Type: <span className='ml-2 text-headingColor text-[22px] leading-8'>O-</span></p>
+            </div>
+
+            <div className='mt-[50px] md:mt-[100px]'>
+              <button className='w-full bg-[#181A1E] p-3 text-[16px] leading-7 rounded-md text-white'>Logout</button>
+              <button className='w-full bg-red-600 mt-4 p-3 text-[16px] leading-7 rounded-md text-white'>Delete Account</button>
+            </div>
           </div>
 
         </div>
