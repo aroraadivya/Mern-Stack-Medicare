edcc9d279457b6da47ffda1528e44b3f458b17bb
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-18 09:13:32 +0530

    Add useFetchData hook and components to MyBookings for data fetching and error handling

--- frontend/src/Dashboard/user-account/MyBookings.jsx
+++ frontend/src/Dashboard/user-account/MyBookings.jsx
@@ -1,6 +1,14 @@
-import React from 'react'
+import React from 'react';
+import useFetchData from '../../hooks/useFetchData';
+import { BASE_URL } from '../../config';
+import DoctorCard from '../../components/Doctors/DoctorCard';
+import Loading from '../../components/Loader/Loading';
+import Error from '../../components/Error/Error';
 
 const MyBookings = () => {
+
+    const {data:appointments} = useFetchData(`${BASE_URL}`);
+
   return (
     <div>
         MyBookings;
