b47a0568e13347e75454167ca8d459e6868b0cf6
aroraadivya <111882666+aroraadivya@users.noreply.github.com>
2024-12-18 08:55:31 +0530

    Fix formatting issues in Loading component

--- frontend/src/components/Loader/Loading.js
+++ frontend/src/components/Loader/Loading.js
@@ -1,11 +1,11 @@
-import React from 'react'
+;import React from 'react'
 
 const Loading = () => {
   return (
     <div>
       Loading
     </div>
-  )
-}
+  );
+};
 
-export default Loading
+export default Loading;
